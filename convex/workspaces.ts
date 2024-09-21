52b10fde8f287d4bceb73d0250128ede56a02ee4
Valentyn Kasyan <vkasyan@gmx.de>
2024-09-21 09:30:53 +0200

    Workspace Members || Attempt to resolve "Cannot update a component (`HotReload`) while rendering a different component (`Toolbar`)." error"

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,6 +3,14 @@ import { v } from "convex/values";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { mutation, query } from "./_generated/server";
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "1234567890abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
+};
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -13,7 +21,8 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
     // TODO: create a proper methd later
-    const joinCode = "123456";
+    // const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
@@ -24,16 +33,47 @@ export const create = mutation({
     // in case you need the moment of creation
     // const workspace = await ctx.db.get(workspaceId);
 
+    // If we create workspace – we are admin:
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
     return workspaceId;
   },
 });
 
 // return all possible workspaces:
+// only show the workspaces that the user is a part of:
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    // return await ctx.db.query("workspaces").collect();
+
+    return workspaces;
   },
 });
 
@@ -45,6 +85,18 @@ export const getById = query({
     const userId = await getAuthUserId(ctx);
     if (!userId) {
       throw new Error("Unauthorized");
+    }
+
+    // only one user with user id and workspace id can exist
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
