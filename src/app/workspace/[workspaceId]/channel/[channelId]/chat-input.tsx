9067a8a07b7bd0630180e01257cd2a3a127f186c
Valentyn Kasyan <vkasyan@gmx.de>
2024-10-11 13:17:32 +0200

    Image Upload Feature

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,10 +1,12 @@
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
 import { useRef, useState } from "react";
 import { toast } from "sonner";
+import { Id } from "../../../../../../convex/_generated/dataModel";
 // import { Editor } from "@/components/editor";
 // const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -15,6 +17,13 @@ const Editor = dynamic(
   { ssr: false }
 );
 
+type CreateMessageValues = {
+  channelId: Id<"channels">;
+  workspaceId: Id<"workspaces">;
+  body: string;
+  image: Id<"_storage"> | undefined;
+};
+
 interface ChatInputProps {
   placeholder: string;
 }
@@ -22,9 +31,11 @@ interface ChatInputProps {
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   // in order to control the Editor outside of the component (e.g after we sent the message, we want to clear the editor):
   const editorRef = useRef<Quill | null>(null);
+
   const workspaceId = useWorkspaceId();
   const channelId = useChannelId();
   const { mutate: createMessage } = useCreateMessage();
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
 
   // Everytime the key changes – editor component will be destroyed and rebuilt. Forced rerendering.
   const [editorKey, setEditorKey] = useState<number>(0);
@@ -41,12 +52,47 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
     // console.log({ body, image });
     try {
       setIsPending(true);
+      editorRef.current?.enable(false);
+
+      const values: CreateMessageValues = {
+        channelId,
+        workspaceId,
+        body,
+        image: undefined,
+      };
+
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+
+        console.log("url", { url });
+
+        if (!url) {
+          throw new Error("Failed to generate upload url");
+        }
+
+        const result = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": image.type },
+          body: image,
+        });
+
+        console.log("result", { result });
+
+        if (!result.ok) {
+          throw new Error("Failed to upload image");
+        }
+
+        const { storageId } = await result.json();
+        values.image = storageId;
+      }
+
       await createMessage(
-        {
-          workspaceId,
-          channelId,
-          body,
-        },
+        // {
+        //   workspaceId,
+        //   channelId,
+        //   body,
+        // }
+        values,
         { throwError: true }
         // the function does not throw an error by default, any issues during message creation might not be caught
       );
@@ -56,6 +102,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
       toast.error("Failed to send message");
     } finally {
       setIsPending(false);
+      editorRef.current?.enable(true);
     }
   };
 
